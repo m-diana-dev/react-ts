111e84d4b217e3751ebf57edcc97eeb01b2f99c1
m-diana-dev <m.diana.dev@gmail.com>
2023-06-26 13:30:10 +0300

    13 - controlled input, checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,10 +1,8 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import React, {useRef, useState} from "react";
-import {Rating, RatingValueType} from "../components/Rating/Rating";
+import type {Meta, StoryObj} from '@storybook/react';
+import React, {ChangeEvent, useRef, useState} from "react";
 
 const meta = {
     title: 'Uncontrolled Components/input',
-    // component: UncontrolledAccordion,
     tags: ['autodocs'],
 } satisfies Meta;
 
@@ -14,24 +12,24 @@ type Story = StoryObj
 
 
 export const Input: Story = {
-    render: function Render () {
+    render: function Render() {
         return <input value={'js'}/>
     }
 }
 export const UncontrolledInput: Story = {
-    render: function Render () {
+    render: function Render() {
         return <input/>
     }
 }
 export const TrackValueOfUncontrolledInput: Story = {
-    render: function Render () {
+    render: function Render() {
         const [value, setValue] = useState('');
-        return <div><input onChange={(e)=>setValue(e.currentTarget.value)}/> {value}</div>
+        return <div><input onChange={(e) => setValue(e.currentTarget.value)}/> {value}</div>
     }
 }
 
 export const TrackValueOfUncontrolledInputWithRef: Story = {
-    render: function Render () {
+    render: function Render() {
         const [value, setValue] = useState('');
         const refInput = useRef<HTMLInputElement>(null);
         const saveValue = () => {
@@ -40,7 +38,43 @@ export const TrackValueOfUncontrolledInputWithRef: Story = {
         }
         return <div>
             <input ref={refInput}/>
-            <button onClick={saveValue}>Click</button> {value}
+            <button onClick={saveValue}>Click</button>
+            {value}
+        </div>
+    }
+}
+
+export const ControlledInput: Story = {
+    render: function Render() {
+        const [value, setValue] = useState('');
+        const OnChange = (e: ChangeEvent<HTMLInputElement>) => {
+            setValue(e.currentTarget.value)
+        }
+        return <div><input value={value} onChange={OnChange}/> {value}</div>
+    }
+}
+export const ControlledCheckbox: Story = {
+    render: function Render() {
+        const [value, setValue] = useState(false);
+        const OnChange = (e: ChangeEvent<HTMLInputElement>) => {
+            setValue(e.currentTarget.checked)
+        }
+        return <div><input type={"checkbox"} checked={value} onChange={OnChange}/> </div>
+    }
+}
+
+export const ControlledSelect: Story = {
+    render: function Render() {
+        const [value, setValue] = useState('2');
+        const OnChange = (e: ChangeEvent<HTMLSelectElement>) => {
+            setValue(e.currentTarget.value)
+        }
+        return <div>
+            <select value={value} onChange={OnChange}>
+                <option value={'1'}>1</option>
+                <option value={'2'}>2</option>
+                <option value={'3'}>3</option>
+            </select>
         </div>
     }
 }
