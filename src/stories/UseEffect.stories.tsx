11dc7a424bbbbef7fad79783ec32045198fccf94
m-diana-dev <m.diana.dev@gmail.com>
2023-09-02 17:06:39 +0300

    my clock story

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -16,27 +16,27 @@ export const example1: Story = {
         const [value, setValue] = useState(0);
         const [fake, setFake] = useState(0);
 
-        useEffect(()=>{
+        useEffect(() => {
             console.log('useEffect every render')
             //side effects
         })
 
-        useEffect(()=>{
+        useEffect(() => {
             console.log('useEffect only firs render (componentDidMount)')
             //side effects
             document.title = value.toString()
         }, [])
 
-        useEffect(()=>{
+        useEffect(() => {
             console.log('useEffect firs render and every value change')
             //side effects
         }, [value])
 
         return <div>
             <h1>Hello!</h1>
-            <button onClick={() => setValue(value+1)}>Click Value</button>
+            <button onClick={() => setValue(value + 1)}>Click Value</button>
             <span>{value}</span>
-            <button onClick={() => setFake(fake+1)}>Click Fake</button>
+            <button onClick={() => setFake(fake + 1)}>Click Fake</button>
             <span>{fake}</span>
         </div>
     }
@@ -49,9 +49,9 @@ export const SetTimeOutExample: Story = {
         const [fake, setFake] = useState(0);
 
 
-        useEffect(()=>{
+        useEffect(() => {
             //side effects
-            setTimeout(()=>{
+            setTimeout(() => {
                 console.log('set time out')
                 document.title = value.toString()
             }, 1000)
@@ -59,9 +59,9 @@ export const SetTimeOutExample: Story = {
 
         return <div>
             <h1>Hello!</h1>
-            <button onClick={() => setValue(value+1)}>Click Value</button>
+            <button onClick={() => setValue(value + 1)}>Click Value</button>
             <span>{value}</span>
-            <button onClick={() => setFake(fake+1)}>Click Fake</button>
+            <button onClick={() => setFake(fake + 1)}>Click Fake</button>
             <span>{fake}</span>
         </div>
     }
@@ -73,11 +73,11 @@ export const SetIntervalExample: Story = {
         const [fake, setFake] = useState(0);
 
 
-        useEffect(()=>{
+        useEffect(() => {
             //side effects
-            setInterval(()=>{
+            setInterval(() => {
                 console.log('tick' + value)
-                setValue(state=>state+1)
+                setValue(state => state + 1)
             }, 1000)
         }, [])
 
@@ -85,6 +85,52 @@ export const SetIntervalExample: Story = {
             <h1>Hello!</h1>
             <span>value: {value}</span>
             <span>fake: {fake}</span>
+        </div>
+    }
+}
+
+export const clockExample: Story = {
+
+    render: function Render() {
+
+        let date = new Date();
+        const [hours, setHours] = useState(date.getHours());
+        const [minutes, setMinutes] = useState(date.getMinutes());
+        const [seconds, setSeconds] = useState(date.getSeconds());
+
+        useEffect(() => {
+            setSeconds(date.getSeconds())
+            setMinutes(date.getMinutes())
+            setHours(date.getHours())
+        }, [])
+
+        useEffect(() => {
+            setTimeout(() => {
+                setSeconds(second => second + 1)
+            }, 1000)
+        }, [seconds])
+        useEffect(() => {
+            if (seconds === 60) {
+                setSeconds(0)
+                setMinutes(minute => minute + 1)
+            }
+        }, [seconds])
+        useEffect(() => {
+            if (minutes === 59 && seconds === 60) {
+                setMinutes(0)
+                setHours(hours => hours + 1)
+            }
+        }, [minutes, seconds])
+        useEffect(() => {
+            if (hours === 23 && minutes === 59 && seconds === 60) {
+                setSeconds(0)
+                setMinutes(0)
+                setHours(0)
+            }
+        }, [minutes, seconds, hours])
+
+        return <div>
+            {hours}:{minutes}:{seconds}
         </div>
     }
 }
